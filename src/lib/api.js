f8f86ff6f00523c40c2ca0f308ab0cd2d1baac1e
Dharmraj07 <dharmrajh03@gmail.com>
2026-09-20 14:38:43 +0530

    change the url

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "https://auth.febrocket.com";
+  "https://auth-service-441625071558.asia-south1.run.app";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
